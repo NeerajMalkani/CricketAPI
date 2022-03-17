605bcb97c22168d1a318b4a1f2541cada32af805
Neeraj Malkani <neerajmalkani33@gmail.com>
2022-03-17 19:37:30 +0530

    API key authentication added

---
+++ CricketAPI/Helpers/ApiKeyMiddleware.cs
@@ -0,0 +1,42 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using System.Net;
+
+namespace CricketAPI.Helpers
+{
+    public class ApiKeyMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private
+        const string APIKEY = "XApiKey";
+        public ApiKeyMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+        public async Task InvokeAsync(HttpContext context)
+        {
+            Response response = new Response();
+            if (!context.Request.Headers.TryGetValue(APIKEY, out
+                    var extractedApiKey))
+            {
+                Common.CreateResponse(HttpStatusCode.Unauthorized, "Unauthorized", "API Key not provided", out response);
+                var serializerSettings = new JsonSerializerSettings();
+                serializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+                serializerSettings.Formatting = Formatting.Indented;
+                await context.Response.WriteAsync(JsonConvert.SerializeObject(response, serializerSettings));
+                return;
+            }
+            var appSettings = context.RequestServices.GetRequiredService<IConfiguration>();
+            var apiKey = appSettings.GetValue<string>(APIKEY);
+            if (!apiKey.Equals(extractedApiKey))
+            {
+                Common.CreateResponse(HttpStatusCode.Unauthorized, "Unauthorized", "Authorization has been denied for this request", out response);
+                var serializerSettings = new JsonSerializerSettings();
+                serializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+                await context.Response.WriteAsync(JsonConvert.SerializeObject(response, serializerSettings));
+                return;
+            }
+            await _next(context);
+        }
+    }
+}
--- CricketAPI/Program.cs
+++ CricketAPI/Program.cs
@@ -11,6 +11,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddTransient<MySqlConnection>(_ => new MySqlConnection(builder.Configuration["ConnectionStrings:Default"]));
 var app = builder.Build();
+app.UseMiddleware<ApiKeyMiddleware>();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -41,21 +42,28 @@ app.MapGet("/getcontinents", () =>
 #region Fixtures
 app.MapGet("/getfixtures", () =>
 {
-    MySqlConnection connection = MySQLHelper.EstablishConnection(app.Configuration);
-    MySqlCommand command = MySQLHelper.ExecuteCommand(connection, "GetFixtures", CommandType.StoredProcedure);
-    MySqlDataReader reader = command.ExecuteReader();
     Response response = new Response();
-    List<Fixtures> arrfixtures = new List<Fixtures>();
-    while (reader.Read())
+    try
     {
-        FixtureDetails fixturedetails = Common.ConvertToObject<FixtureDetails>(reader);
-        FixtureToss fixturetoss = Common.ConvertToObject<FixtureToss>(reader);
-        Fixtures? fixtures = Common.ConvertToObject<Fixtures>(reader);
-        fixtures.fixture_details = fixturedetails;
-        fixtures.fixture_toss = fixturetoss;
-        arrfixtures.Add(fixtures);
+        List<Fixtures> arrfixtures = new List<Fixtures>();
+        MySqlConnection connection = MySQLHelper.EstablishConnection(app.Configuration);
+        MySqlCommand command = MySQLHelper.ExecuteCommand(connection, "GetFixtures", CommandType.StoredProcedure);
+        MySqlDataReader reader = command.ExecuteReader();
+        while (reader.Read())
+        {
+            FixtureDetails fixturedetails = Common.ConvertToObject<FixtureDetails>(reader);
+            FixtureToss fixturetoss = Common.ConvertToObject<FixtureToss>(reader);
+            Fixtures? fixtures = Common.ConvertToObject<Fixtures>(reader);
+            fixtures.fixture_details = fixturedetails;
+            fixtures.fixture_toss = fixturetoss;
+            arrfixtures.Add(fixtures);
+        }
+        Common.CreateResponse(HttpStatusCode.OK, "Success", "Success", out response, arrfixtures);
     }
-    Common.CreateResponse(HttpStatusCode.OK, "Success", "Success", out response, arrfixtures);
+    catch (Exception ex)
+    {
+        Common.CreateErrorResponse(HttpStatusCode.BadRequest, out response, ex);
+    }
     return response;
 })
 .WithName("GetFixtures");
